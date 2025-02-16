b96dea6acf293059e2ac1660f2df2adbb520352a
yuxunn <teeyuxun@gmail.com>
2025-02-16 19:39:26 +0800

    minor changes

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -3,27 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 const redemptionRoutes_1 = __importDefault(require("./routes/redemptionRoutes"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
-app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
-    methods: "GET,POST",
-    allowedHeaders: "Content-Type"
-}));
+app.use((0, cors_1.default)({ origin: "https://govtech-assignment-daofpfo8x-yuxunns-projects.vercel.app" }));
 app.use(express_1.default.json());
-const frontendBuildPath = path_1.default.join(__dirname, "../../frontend/build");
-app.use(express_1.default.static(frontendBuildPath));
 app.get("/", (req, res) => {
-    res.sendFile(path_1.default.join(frontendBuildPath, "index.html"));
+    res.send("Welcome to the GovWallet Redemption API.");
 });
 app.use("/api", redemptionRoutes_1.default);
-app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(frontendBuildPath, "index.html"));
-});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
