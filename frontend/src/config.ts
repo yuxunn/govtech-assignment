f898b215a4b8b6c1e6f84f15d9a320ce6f4937a1
yuxunn <teeyuxun@gmail.com>
2025-02-16 19:49:26 +0800

    minor changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001/api";
-
+const API_BASE_URL = "https://govwallet-backend.onrender.com/api";
 export default API_BASE_URL;
